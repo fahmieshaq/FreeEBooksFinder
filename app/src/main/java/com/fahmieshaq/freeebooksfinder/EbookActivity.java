001a70566a7212e07076f359b8a9314d16d86549
Fahmi Eshaq <fjamcom@gmail.com>
2017-07-28 00:10:04 -0400

    Initial commit. Newly created project. No work has been done yet

---
+++ app/src/main/java/com/fahmieshaq/freeebooksfinder/EbookActivity.java
@@ -0,0 +1,13 @@
+package com.fahmieshaq.freeebooksfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EbookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ebook);
+    }
+}
