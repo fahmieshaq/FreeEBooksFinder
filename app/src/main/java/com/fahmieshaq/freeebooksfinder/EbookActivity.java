ac9c016c6b72d24db648a3b5b883810bde68f2c0
Fahmi Eshaq <fjamcom@gmail.com>
2017-07-28 00:16:48 -0400

    Edited main activity layout and created a list item layout

--- app/src/main/java/com/fahmieshaq/freeebooksfinder/EbookActivity.java
+++ app/src/main/java/com/fahmieshaq/freeebooksfinder/EbookActivity.java
@@ -11,3 +11,15 @@ public class EbookActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ebook);
     }
 }
+
+
+//https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&orderBy=relevance&key=yourAPIKey
+
+
+//AIzaSyB20aZ9XkAkt4YTPZWDEG71tqPsPJZ0QxM
+
+//Root (object) -> items (array) -> array index 0 is object -> volumeInfo property object
+// -> title property -> published date property -> categories propery has an array with one index
+// -> imageLinks property (object) -> thumbnail property
+// -> preview link property -> info link property (Read Now)
+// -> PDF (object) -> downloadLink
